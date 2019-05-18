0897ca1dcd310ccfd2d28778d8fbd984f5b0a032
QuanTranDoanAnh <td.anhquan@gmail.com>
2019-05-18 20:48:48 +0700

    Requirement 1 - check boundaries and invalid places

--- src/main/java/vn/quantda/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/vn/quantda/tddjava/ch03tictactoe/TicTacToe.java
@@ -1,5 +1,36 @@
 package vn.quantda.tddjava.ch03tictactoe;
 
 public class TicTacToe {
+	private Character[][] board = {
+			{'\0', '\0', '\0'},
+			{'\0', '\0', '\0'},
+			{'\0', '\0', '\0'}
+	};
+	
+	public void play(int x, int y) {
+		checkXAxis(x);
+		checkYAxis(y);
+		setBox(x, y);
+	}
+
+	private void setBox(int x, int y) {
+		if (board[x -1][y - 1] != '\0') {
+			throw new RuntimeException("Box is occupied");
+		} else {
+			board[x -1][y - 1] = 'X';
+		}
+	}
+
+	private void checkYAxis(int y) {
+		if (y <1 || y > 3) {
+			throw new RuntimeException("Y is outside board");
+		}
+	}
+
+	private void checkXAxis(int x) {
+		if (x < 1 || x > 3) {
+			throw new RuntimeException("X is outside board");
+		}
+	}
 
 }
